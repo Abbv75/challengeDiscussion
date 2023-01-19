3f58a2e5fa9f4998eaa8eb27e0c4afab19d7c058
abbv75 <bore.younous59@gmail.com>
2023-01-19 12:47:39 +0000

    ajoue de la nav

---
+++ style/js/navbar.js
@@ -0,0 +1,5 @@
+$('.navToggler').click(function (e) { 
+    $('.fa-arrow-left').toggleClass('closed');    
+    $('.navBarZone nav').toggleClass('navClosed');    
+    // $('.navBarZone nav').slideToggle();
+});
